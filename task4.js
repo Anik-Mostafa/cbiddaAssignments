e3dac1f654d3cd0645335b569e863a7435ee693c
anik_mostafa <mohshinmostafa@gmail.com>
2019-06-30 17:19:35 +0600

    After realy hard days

---
+++ task4.js
@@ -0,0 +1,18 @@
+function bar (callback) {
+     foo(function (err, data) {  
+       if (err)
+         return callback(err)
+
+
+           for (i=0;i<x.length;i++){
+             if(.search(title)>-1){
+               console.log();
+             }
+           }
+
+
+       callback(null, data)
+     })
+   }
+
+function find()
